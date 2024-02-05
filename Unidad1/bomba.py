44c6c4da325b529a29e25a2385e7027fada8e30d
Juan Fernando Franco Higuita <juanferfranco@gmail.com>
2024-02-05 12:12:27 -0500

    Create bomba.py

---
+++ Unidad1/bomba.py
@@ -0,0 +1,9 @@
+# Imports go at the top
+from microbit import *
+
+
+# Code in a 'while True:' loop repeats forever
+while True:
+    display.show(Image.HEART)
+    sleep(1000)
+    display.scroll('Hello')
