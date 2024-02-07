e499edc007ea0c2d017e1026875c7b8a236deb05
WaterLemur <38868316+LemurWater@users.noreply.github.com>
2024-02-07 13:12:09 -0500

    cambiado los valores de 0-1
    Mayor coherencia con otros sistemas

--- Unidad1/src/io-read.py
+++ Unidad1/src/io-read.py
@@ -5,6 +5,6 @@ from microbit import *
 # Code in a 'while True:' loop repeats forever
 while True:
     if pin0.is_touched():
-        display.show(0)
-    else:
         display.show(1)
+    else:
+        display.show(0)
