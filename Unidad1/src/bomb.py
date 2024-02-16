69f39ee0baf59f34b1b27e3d406c84c6ff008c03
WaterLemur <38868316+LemurWater@users.noreply.github.com>
2024-02-16 07:13:00 -0500

    Update bomb.py

--- Unidad1/src/bomb.py
+++ Unidad1/src/bomb.py
@@ -3,18 +3,41 @@ from microbit import *
 
 
 # Code in a 'while True:' loop repeats forever
-countdown = 9
-state = "waitting"
+state = "DISARMED"
+flag = "waitting"
+
+
+def config(timer):
+    if button_a.is_pressed():
+        if button_b.is_pressed():
+            state = "armed"
+            flag = "2"
+            timer = 0
+        else:
+            if timer > 1:
+                countdown = timer - 1
+            #falta serial
+    elif button_b.was_pressed():
+        if timer < 9:
+            countdown = timer + 1
+            #falta serial
+    #Display
+    display.show(timer)
+
+def countdown(timer):
+    #Display
+    display.show(timer)
+
+    #falta serial
+
+    #timer
+    countdown = timer - 1
+
+
 
 while True:
-    #Config
-    if button_a.was_pressed():
-        if countdown > 1:
-            countdown = countdown -1
-
-    elif button_b.was_pressed():
-        if countdown < 9:
-            countdown = countdown +1
-
-    #Display
-    display.show(countdown)
+    if state == "DISSARMED":
+        #config timer
+        config(countdown)
+    elif state == "ARMED":
+        countdown(countdown)
