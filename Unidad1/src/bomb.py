afdd25bbcf8d506a2d9f25cfd7b3b7d02ab3b2e2
WaterLemur <38868316+LemurWater@users.noreply.github.com>
2024-03-01 09:13:39 -0500

    VERSION GIGA-ESTABLE

--- Unidad1/src/bomb.py
+++ Unidad1/src/bomb.py
@@ -6,12 +6,12 @@ import utime
 
 
 state = 'SETUP'
-timer = 20
+timer =  20
 DELAY = 75
 startTime = utime.ticks_us()
 
 
-dcode = ["UP", "DOWN", "UP", "DOWN", "UP", "UP", "ARMED"]
+dcode = ["ARMED", "ARMED", "ARMED", "ARMED", "ARMED", "ARMED", "ARMED"]
 userInput = ["UP", "UP", "UP", "UP", "UP", "UP", "ARMED"]
 userInputPtr = 0
 
@@ -44,10 +44,14 @@ def _draw_arrow():
     
 def setup():
     global state
-
+    global dcode
+    global userInputPtr
+    
     speech.say('SETUP')
     state = 'SETUP'
     set_volume(255)
+    dcode = ["ARMED", "ARMED", "ARMED", "ARMED", "ARMED", "ARMED", "ARMED"]
+    userInputPtr = 0
     _draw_arrow()
 
     
@@ -55,6 +59,12 @@ def setup():
     state = 'CONFIG'
     speech.say('CONFIG')
     
+
+def clear_input():
+    if button_a.was_pressed():
+        display.show(Image.HAPPY)
+    if button_b.was_pressed():
+        display.show(Image.HAPPY)
     
 def config():
     global state
@@ -65,7 +75,11 @@ def config():
         state = 'ARMED'
         speech.say('ARMED')
         startTime = utime.ticks_us()
+        clear_input()
         display.scroll(timer, delay=DELAY, loop=False, wait=False)
+        sleep(1000)
+        display.show(userInputPtr)
+        sleep(300)
         return
     if button_a.is_pressed():
         if timer > 10:
@@ -79,6 +93,8 @@ def config():
             music.play(['d'])
             display.scroll(timer, delay=DELAY, loop=True, wait=False)
             #falta serial
+
+
 
 def countdown():
     global state
@@ -138,38 +154,34 @@ def io_read():
 
 def input_listener():
     user_input()
-    #cable_disarm()
-
+    #cable_disarm()3.
+        
 def user_input():
     global userInputPtr
+    global state
     
     if button_a.was_pressed():
-        display.show(Image.ARROW_S)
         userInput[userInputPtr] = 'DOWN'
         userInputPtr += 1
-        display.show(Image.ARROW_S)
+        display.show(userInputPtr)
             
     elif button_b.was_pressed():
         userInput[userInputPtr] = 'UP'
         userInputPtr += 1
-        display.show(Image.ARROW_N)
+        display.show(userInputPtr)
             
     elif pin_logo.is_touched():
-        if userInputPtr < 5:
-            userInput[userInputPtr] = 'ARMED'
-            userInputPtr += 1
-        else:
-            userInputPtr = 0
+        userInput[userInputPtr] = 'ARMED'
+        userInputPtr += 1
+        display.show('A')
 
     if userInputPtr > 6:
-        global state
-        
-        display.show(Image.NO)
         if userInput == dcode:
             state = 'DISARMED'
             display.show(Image.YES)
-            sleep(500)
+            sleep(1250)
         else:
+            display.show(Image.NO)
             userInputPtr = 0
         #for i in range(7):
             #if userInput[i] != dcode[i]:
@@ -203,7 +215,7 @@ setup()
 # Code in a 'while True:' loop repeats forever
 while True:
     if state == 'SETUP':
-     setup()
+        setup()
     elif state == 'CONFIG':
         config()
     elif state == 'ARMED':
