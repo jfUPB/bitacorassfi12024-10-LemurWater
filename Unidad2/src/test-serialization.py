cdad6ad06f9382ead86593b74c89d1ac536f8edf
Lemus Watler <lemusdwe@gmail.com>
2024-03-13 13:27:45 -0500

    src

---
+++ Unidad2/src/test-serialization.py
@@ -0,0 +1,22 @@
+from microbit import *
+
+uart.init(baudrate=115200)
+display.show(Image.BUTTERFLY)
+
+while True:
+    if button_a.is_pressed():
+        uart.write('A')
+        sleep(500)
+    if button_b.is_pressed():
+        uart.write('B')
+        sleep(500)
+    if accelerometer.was_gesture('shake'):
+        uart.write('C')
+        sleep(500)
+    if uart.any():
+        data = uart.read(1)
+        if data:
+            if data[0] == ord('h'):
+                display.show(Image.HEART)
+                sleep(500)
+                display.show(Image.HAPPY)
