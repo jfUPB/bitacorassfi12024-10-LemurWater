7e8c9d8cdd637361d3b815609dcf47656e47d4b9
WaterLemur <38868316+LemurWater@users.noreply.github.com>
2024-04-13 11:07:53 -0700

    enviar en un solo mensaje MICROBIT

--- Unidad2/src/microbit.py
+++ Unidad2/src/microbit.py
@@ -2,25 +2,36 @@ from microbit import *
 
 uart.init(baudrate=115200)
 display.show(Image.BUTTERFLY)
+uart_buffer =''
 
 while True:
-    # Send
+    # Pause
+    if pin_logo.is_touched():
+        uart_buffer = uart_buffer + 'P'
+        # uart.write('P')
+        
+    # Accelerometer  
+    if accelerometer.was_gesture('shake'):
+         x_strength = accelerometer.get_x()
+         uart_buffer = uart_buffer + 'X'
+         # uart.write('X')
+   
+    # Button press
     if button_a.was_pressed():
-        uart.write('A')
+        uart_buffer = uart_buffer + 'A'
+        # uart.write('A')
     if button_b.was_pressed():
-        uart.write('D')
+        uart_buffer = uart_buffer + 'D'
+        # uart.write('D')
+    
+    # Send
+    if len(uart_buffer) > 0:
+        uart.write(uart_buffer)
+        uart_buffer = ''
+
     # Read
     if uart.any():
         data = uart.read(1)
         if data:
             if data[0] == ord('h'):
                 display.show(Image.HEART)
-        
-    # Pause
-    if pin_logo.is_touched():
-        uart.write('P')
-        
-    # Speed  
-    if accelerometer.was_gesture('shake'):
-         x_strength = accelerometer.get_x()
-         uart.write('X')
