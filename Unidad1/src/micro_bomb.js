39d9fc73c5b50616f6bc9226b9a24fa1cc110c46
WaterLemur <38868316+LemurWater@users.noreply.github.com>
2024-04-22 17:07:04 -0700

    Update micro_bomb.js

--- Unidad1/src/micro_bomb.js
+++ Unidad1/src/micro_bomb.js
@@ -1,3 +1,6 @@
+let port;
+let connectBtn;
+// --------------------------------------------------------------------
 let state = 0;
 
 let canvasWidth = 800;
@@ -9,11 +12,22 @@ let canvasColor = 40;
 function setup() {
   setWindow();
   intro();
+
+  port = createSerial();
+  connectBtn = createButton("Connect to micro:bit");
+  connectBtn.position(300, 345);
+  connectBtn.mousePressed(connectBtnClick);
 }
 // --------------------------------------------------------------------
 
 // --------------------------------------------------------------------
-function draw() {}
+function draw() {
+  if (!port.opened()) {
+    connectBtn.html("Connect to micro:bit");
+  } else {
+    connectBtn.html("Disconnect");
+  }
+}
 // --------------------------------------------------------------------
 
 // --------------------------------------------------------------------
@@ -25,8 +39,17 @@ function setWindow() {
 function intro() {
   textSize(300);
   text("🧨", 250, 270);
-  ellipse(canvasWidth/2, 190, 250, 80);
+  ellipse(canvasWidth / 2, 190, 250, 80);
   textSize(42);
-  text("micro:bomb", (canvasWidth/2) - 100, (canvasHeight/2) - 0);
+  text("micro:bomb", canvasWidth / 2 - 100, canvasHeight / 2 - 0);
 }
+
+function connectBtnClick() {
+  if (!port.opened()) {
+    port.open("MicroPython", 115200);
+  } else {
+    port.close();
+  }
+}
+
 // --------------------------------------------------------------------
