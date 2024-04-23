e0d62201db42fcdfaee230b2cdd1b664e76c6cbd
WaterLemur <38868316+LemurWater@users.noreply.github.com>
2024-04-22 19:42:56 -0700

    Update micro_bomb.js

--- Unidad1/src/micro_bomb.js
+++ Unidad1/src/micro_bomb.js
@@ -1,32 +1,16 @@
+// micro_bomb.js
+// --------------------------------------------------------------------
 let port;
 let connectBtn;
-// --------------------------------------------------------------------
+
 let state = 1;
-
-let canvasWidth = 800;
-let canvasHeight = 400;
-let canvasColor = 40;
-
-let display1 = "?";
-let display2 = "=";
-let display3 = "A";
-let display4 = "X";
-let display5 = "Y";
-let display6 = "Z";
-// --------------------------------------------------------------------
-
 // --------------------------------------------------------------------
 function setup() {
   setWindow();
   intro();
 
-  port = createSerial();
-  connectBtn = createButton("Connect to micro:bit");
-  connectBtn.position(300, 345);
-  connectBtn.mousePressed(connectBtnClick);
+  startSerial();
 }
-// --------------------------------------------------------------------
-
 // --------------------------------------------------------------------
 function draw() {
   checkPortState();
@@ -42,69 +26,4 @@ function draw() {
       break;
   }
 }
-// --------------------------------------------------------------------
-
-// --------------------------------------------------------------------
-function setWindow() {
-  createCanvas(canvasWidth, canvasHeight);
-  background(canvasColor);
-}
-
-function intro() {
-  textSize(300);
-  text("🧨", 250, 270);
-  ellipse(canvasWidth / 2, 190, 250, 80);
-  textSize(42);
-  text("micro:bomb", canvasWidth / 2 - 100, canvasHeight / 2 - 0);
-}
-
-function bomb() {
-  // Background
-  background(canvasColor);
-  textSize(300);
-  text("🧨", 250, 270);
-  strokeWeight(15);
-  rect(230, 160, 350, 80);
-
-  // Pixels
-  strokeWeight(0);
-  fill(color(179, 255, 240));
-  rect(245, 173, 45, 55);
-  rect(300, 173, 45, 55);
-  rect(355, 173, 45, 55);
-  rect(410, 173, 45, 55);
-  rect(465, 173, 45, 55);
-  rect(520, 173, 45, 55);
-
-  // Text
-  fill(color(0, 102, 82));
-  textSize(55);
-  text("1", 249, 222);
-  text("0", 306, 222);
-  text("1", 359, 222);
-  text("0", 414, 222);
-  text("1", 470, 222);
-  text("0", 525, 222);
-  fill(color(102, 255, 204));
-}
-function score() {}
-
-function connectBtnClick() {
-  if (!port.opened()) {
-    port.open("MicroPython", 115200);
-  } else {
-    port.close();
-  }
-}
-function checkPortState() {
-  if (!port.opened()) {
-    connectBtn.position(300, 345);
-    connectBtn.html("Connect to micro:bit");
-  } else {
-    state = 1;
-    connectBtn.position(320, 345);
-    connectBtn.html("Disconnect");
-  }
-}
-
 // --------------------------------------------------------------------
