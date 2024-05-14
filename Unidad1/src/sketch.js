15ea34e9e29c62b7afc8241259a7a8e5fbe10367
WaterLemur <38868316+LemurWater@users.noreply.github.com>
2024-05-13 21:50:10 -0500

    Update sketch.js

--- Unidad1/src/sketch.js
+++ Unidad1/src/sketch.js
@@ -1,29 +1,33 @@
-// micro_bomb.js
-// --------------------------------------------------------------------
 let port;
 let connectBtn;
 
-let state = 1;
 // --------------------------------------------------------------------
 function setup() {
-  setWindow();
-  intro();
-
-  startSerial();
+  initialize();
 }
 // --------------------------------------------------------------------
 function draw() {
   checkPortState();
-
   switch (state) {
     case 0:
+      initialize();
       break;
     case 1:
-      bomb();
+      _setup_();
       break;
     case 2:
-      score();
+      configure();
+      break;
+    case 3:
+      countdown();
+      break;
+    case 4:
+      explode();
+      break;
+    case 5:
+      disarm();
       break;
   }
+  console.log("state = " + state);
 }
 // --------------------------------------------------------------------
