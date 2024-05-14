8ca66fda5e39218fc3dd93d249f8647eb6435762
WaterLemur <38868316+LemurWater@users.noreply.github.com>
2024-05-13 21:49:21 -0500

    Update serial.js

--- Unidad1/src/modules/serial.js
+++ Unidad1/src/modules/serial.js
@@ -1,12 +1,10 @@
-// serial_ascii.js
-// --------------------------------------------------------------------
 function startSerial() {
   port = createSerial();
   connectBtn = createButton("Connect to micro:bit");
   connectBtn.position(300, 345);
   connectBtn.mousePressed(connectBtnClick);
 }
-
+// --------------------------------------------------------------------
 function connectBtnClick() {
   if (!port.opened()) {
     port.open("MicroPython", 115200);
@@ -14,15 +12,33 @@ function connectBtnClick() {
     port.close();
   }
 }
+// --------------------------------------------------------------------
 function checkPortState() {
   if (!port.opened()) {
     connectBtn.position(300, 345);
     connectBtn.html("Connect to micro:bit");
   } else {
-    state = 1;
-    console.log("state 1 - bomb");
     connectBtn.position(320, 345);
     connectBtn.html("Disconnect");
   }
+}
+// --------------------------------------------------------------------
+function sendSerial(message) {
+  port.write(message);
+}
+// --------------------------------------------------------------------
+function receiveSerial() {
+  if (port.availableBytes() > 0) {
+    let dataRx = port.read(1);
+    return dataRx;
+  }
+  return null;
+}
+// --------------------------------------------------------------------
+function ThisButton() {
+  background(220);
+  ellipse(width / 2, height / 2, 100, 100);
+  fill("black");
+  text(dataRx, width / 2, height / 2);
 }
 // --------------------------------------------------------------------
