6983395b19b20764a70a5d0c4e2c81f17ccc55b8
WaterLemur <38868316+LemurWater@users.noreply.github.com>
2024-05-13 22:29:20 -0500

    Create sound.js

---
+++ Unidad1/src/modules/sound.js
@@ -0,0 +1,9 @@
+let pathExpl = 'assets/explosion.mp3';
+let soundExpl;
+let reproducing = false;
+// --------------------------------------------------------------------
+function soundExplosion() {
+  //soundFormats('mp3', 'ogg', 'wav');
+  soundExpl.play();
+}
+// --------------------------------------------------------------------
